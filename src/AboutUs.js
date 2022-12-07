1ca7e2c167f4d0063e3b8184a56642df2a1104b9
kelshaaira <kelshairaa@gmail.com>
2022-12-07 21:08:36 +0700

    maps di about us
    sosmed blm, maps blm mau muncul jg

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,26 +1,27 @@
 import Heading from "./components/Heading";
 import Footer from "./components/Footer";
 import "./App.css";
+import Maps from "./components/Maps";
 
 function AboutUs() {
     return(
         <div className="container">
             <Heading/>
-                <h1>About Us</h1>
-                <p class="text-justify">Arranged With Love
+                <h1 class="pt-md-3 pl-md-3 text-justify">About Us</h1>
+                <p>Arranged With Love
 Established in 2017, Fedora Florist is known for our beautiful flower 
 arrangement in fresh, preserved dried and artificial flowers. 
 We also provide unique design for any kind of floral products and events.
 As starting in 2018, we started new fields in bridal bouquet, flower installation 
 and decoration for all events especially wedding. </p>
-            <h1>Address</h1>
+            <h1 class="pt-md-3 pl-md-3 text-justify">Address</h1>
             <p>Jl. Flamboyan utama 1 C2 no 9, Kosambi Baru, Cengkareng, Jakarta Barat 11750</p>
-            <h1>Contact Us</h1>
+            <h1 class="pt-md-3 pl-md-3 text-justify">Contact Us</h1>
             <p>belum tau mau di tulisin apa disini kl pun gaada yaudah</p>
-            <h1>Social Media</h1>
-            
+            <h1 class="pt-md-3 pl-md-3 text-justify">Social Media</h1>
             <Footer/>
-        </div>
+            <Maps/>
+            </div>
     );
 }
 
---
+++ src/components/Maps.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function Maps() {
+    return (
+        <div className="Maps">
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d318.1845605019175!2d106.71323790826833!3d-6.172197416622495!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e69f99ee8119fcb%3A0x88c343a68367cb39!2sFeodora%20Florist!5e0!3m2!1sen!2sid!4v1670420460139!5m2!1sen!2sid"
+            width="600"
+            height="450"
+            style="border:0;"
+            allowfullscreen=""
+            loading="lazy"
+            referrerpolicy="no-referrer-when-downgrade">
+            </iframe>
+        </div>
+      );
+}
+
+export default Maps;
