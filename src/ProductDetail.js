ede59e6683cd34f4196da945440d1bd5d579fd6d
bellashrns <bella.saharani@student.umn.ac.id>
2022-12-10 19:33:29 +0700

    revisi productdetail

--- src/ProductDetail.js
+++ src/ProductDetail.js
@@ -19,28 +19,28 @@ function ProductDetail(props) {
                 </div>
                 <div className="col">
                     <h1>{products[1].name}</h1>
-                    <h4>{products[1].price}</h4>
+                    <h4>Rp {products[1].price}</h4>
                     {/* <p>{products.desc}</p> */}
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    <h5>Message</h5>
                     <form>
-                        <label>Message</label><br />
-                        <input type="text" />
-                    </form><br />
+                        <textarea type="text" ></textarea>
+                    </form>
                     <p>Quantity</p>
                     <div style={{marginTop: -10, marginLeft: -4}} className="row">
-                        <button
-                            onClick={() => setQuantity(quantity + 1)} 
-                            className="dashButtonLight col-1 p-1"
-                        ><i class="fa-solid fa-plus"></i></button>
-                        <h5 className="col-1 text-center mt-2 me-sm-1">{quantity}</h5>
                         <button 
                             onClick={quantity > 1 ? () => setQuantity(quantity - 1) : undefined} 
                             className="dashButtonLight col-1 p-1"
                         ><i class="fa-solid fa-minus"></i></button>
+                        <h5 className="col-1 text-center mt-2 me-sm-1">{quantity}</h5>
+                        <button
+                            onClick={() => setQuantity(quantity + 1)} 
+                            className="dashButtonLight col-1 p-1"
+                        ><i class="fa-solid fa-plus"></i></button>
                     </div>
                     <div className="row pt-3">
                         <button className="dashButtonLight col-lg-3 m-2">Add To Cart</button>
-                        <button className="dashButtonLight col-lg-3 m-2">Ask Us</button>
+                        <button className="dashButtonLight col-lg-3 m-2"><i class="fa-brands fa-lg fa-whatsapp me-2"></i>Ask Us</button>
                     </div>
                 </div>
             </div>
